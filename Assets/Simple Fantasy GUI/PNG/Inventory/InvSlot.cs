6ee8e4bf0e3ae82c4530e6d6fd6f68c6caa814ef
Craig Lovell <craigjlovell1999@gmail.com>
2022-05-10 09:58:37 +1000

    WorkingGrid

--- Assets/Simple Fantasy GUI/PNG/Inventory/InvSlot.cs
+++ Assets/Simple Fantasy GUI/PNG/Inventory/InvSlot.cs
@@ -6,12 +6,25 @@ using UnityEngine.UI;
 public class InvSlot : MonoBehaviour
 {
     public GameObject prefab;
-    public Vector2 invSize = new Vector2 (0,0);
-    public float slotSize;
-    public Vector2 windowSize;
+
+    public Vector2 invSize;
+    public Vector2 slotSize;
+
+    public RectOffset invPadding;
+    public Vector2 invSpacing;
+
+    GridLayoutGroup grid;
+
     // Start is called before the first frame update
     void Start()
     {
+        grid = this.GetComponentInParent<GridLayoutGroup>();
+        grid.constraintCount = (int)invSize.y;
+        grid.cellSize = slotSize;
+        grid.padding = invPadding;
+
+        grid.spacing = invSpacing;
+
         for(int x = 1; x <= invSize.x; x++)
         {
             for (int y = 1; y <= invSize.y; y++)
@@ -19,9 +32,11 @@ public class InvSlot : MonoBehaviour
                 GameObject slot = Instantiate(prefab) as GameObject;
                 slot.transform.parent = this.transform;
                 slot.name = "slot" + x + "_" + y;
-                slot.GetComponent<RectTransform>().anchoredPosition = new Vector3(windowSize.x/ (invSize.x+1)*x, windowSize.y / (invSize.y+1)* -y, 0);
+                
             }
         }
+
+
     }
 
     // Update is called once per frame
